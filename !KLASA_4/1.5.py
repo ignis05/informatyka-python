7b46a32b3ffa8e283f8e20c2eefb57f94f4e70fe
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-09-25 10:26:00 +0400

    Optimized 1.5

--- !KLASA_4/1.5.py
+++ !KLASA_4/1.5.py
@@ -10,32 +10,24 @@ inp = input('Podaj text:\n')
 height = int(input('Podaj wysokosc:\n'))
 
 fence = []
-
+lengths = []
 for i in range(0, height):
     fence.append([])
-
-partial_length = math.floor(len(inp) / (height + height - 2))
-rest = len(inp) % (height + height - 2)
-
-lengths = []
-
-for i in range(0, height):
     lengths.append([])
 
 index = 0
 direction = 1
-
 for x in inp:
     if index + direction == len(lengths) or index + direction < 0:
         direction = 0 - direction
     lengths[index].append(x)
     index += direction
 
-xx = 0
+index = 0
 for i in range(0, height):
     for j in range(0, len(lengths[i])):
-        fence[i].append(inp[xx])
-        xx += 1
+        fence[i].append(inp[index])
+        index += 1
 
 print('fence:')
 print(fence)
@@ -43,7 +35,6 @@ print(fence)
 index = 0
 direction = 1
 out = ''
-
 for x in inp:
     if index + direction == len(fence) or index + direction < 0:
         direction = 0 - direction
