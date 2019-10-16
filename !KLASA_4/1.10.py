c56ade9a14fdd6264901d93b40c622e7fcf979dd
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-10-16 08:53:43 +0400

    Added 1.9 and 1.10

---
+++ !KLASA_4/1.10.py
@@ -0,0 +1,26 @@
+# Stosując metodę Cezara zaszyfruj tekst KLAMSTWO MA KROTKIE NOGI. Zastosuj klucz równy 9. Podaj alfabet szyfrowy i kryptogram utajnionej wiadomości.
+# Alfabet szyfrowy i kryptogram wpisz w polu odpowiedzi do zadania, oraz prześlij plik z programem pod nazwą: nazwisko.txt
+
+alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+key = 9
+key = int(input('Podaj klucz:\n'))
+alph2 = alph.copy()
+
+txt = 'klamstwo ma krotkie nogi'
+txt = input('Podaj text:\n')
+txt = txt.lower()
+
+for a in range(0, key):
+    alph2.append(alph2.pop(0))
+
+print(alph)
+print(alph2)
+
+out = []
+for a in txt:
+    try:
+        out.append(alph2[alph.index(a)])
+    except:
+        out.append(a)
+
+print(''.join(out))
